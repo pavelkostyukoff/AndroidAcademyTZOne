184f19c5f8e7811ded7bbc93addcc09703f02bd9
Pavel-pikabu <p.kostyukov@pikabu.ru>
2021-01-15 23:29:09 +0300

    end work 2 tasks

--- app/src/main/java/com/example/androidacademy/MainActivity.kt
+++ app/src/main/java/com/example/androidacademy/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.androidacademy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textview: TextView = findViewById(R.id.first_activity_text_view)
+        textview.setOnClickListener {
+            moveTOnextScreen()
+        }
     }
+    companion object {
+        const val TR_S = "transmittedString"
+        const val TR_I = "transmittedInt"
+        const val TR_B = "transmittedBoolean"
+    }
+    private fun moveTOnextScreen() {
+        val intent = Intent(this, SecondActivity::class.java)
+        val transmittedString = "string to transmit"
+        intent.putExtra(TR_S, transmittedString)
+
+        val transmittedInt = 12
+        intent.putExtra(TR_I, transmittedString)
+
+        val transmittedBoolean = false
+        intent.putExtra(TR_B, transmittedString)
+
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/example/androidacademy/SecondActivity.kt
@@ -0,0 +1,22 @@
+package com.example.androidacademy
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidacademy.MainActivity.Companion.TR_B
+import com.example.androidacademy.MainActivity.Companion.TR_I
+import com.example.androidacademy.MainActivity.Companion.TR_S
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val tr_s = intent.getStringExtra(TR_S)
+        val tr_i = intent.getIntExtra(TR_I, -1)
+        val tr_b = intent.getBooleanExtra(TR_B, false)
+        val textview: TextView = findViewById(R.id.second_activity_text_view)
+        textview.text = tr_s
+    }
+}
